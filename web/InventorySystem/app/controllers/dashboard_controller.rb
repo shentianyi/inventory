b31dac0cb2817792224dfba95b58f7f877dc7c25
Charlot <7wangsong@gmail.com>
2015-11-16 18:43:18 +0800

    chart

--- web/InventorySystem/app/controllers/dashboard_controller.rb
+++ web/InventorySystem/app/controllers/dashboard_controller.rb
@@ -5,9 +5,9 @@ class DashboardController < ApplicationController
     @check_qty_progress = Inventory.where("check_qty != ''").group('date(check_time)').count(:id)
     # @check_qty_progress = Inventory.where("check_qty != ''").group("date_format(check_time, '%Y-%m-%d')").count(:id).map {|check_time, count_id| [check_time, (count_id/Inventory.where("check_qty != ''").count.to_f * 100).round(0)] }
     
-    @check_qty_tmp = @check_qty_progress.collect { |item| {:name => item[0], :y => item[1]} }
+    @check_qty_tmp = @check_qty_progress.collect { |item| {:name => item[0].strftime('%m-%d'), :y => item[1]} }
     @check_qty_tmp << {:name => "未全盘", :y => Inventory.where("check_qty is null").count(:id)}
-    
+    @check_qty_tmp_size=@check_qty_tmp.size
     puts "the hash array json #{@check_qty_tmp}"
     
     # puts @check_qty_progress
@@ -17,11 +17,15 @@ class DashboardController < ApplicationController
     @random_check_qty_progress = Inventory.random_check_completed.group('date(random_check_time)').count(:random_check_qty)
     # @random_check_qty_progress['未抽盘'] = Inventory.random_check.count(:id)
     # @random_check_qty_progress['未抽盘'] = Inventory.random_check_not.count(:id)
-    @random_check_qty_tmp = @random_check_qty_progress.collect { |item| {:name => item[0], :y => item[1]} }
+    @random_check_qty_tmp = @random_check_qty_progress.collect { |item| {:name => item[0].strftime('%m-%d'), :y => item[1]} }
     @random_check_qty_tmp << {:name => "未抽盘", :y => Inventory.random_check_not.count(:id)}
-    
-    @check_qty_hour_progress = Inventory.where("check_qty != ''").group("date_format(check_time, '%Y-%m-%d %H点')").count(:id)
-    @random_check_qty_hour_progress = Inventory.random_check_completed.group("date_format(random_check_time, '%Y-%m-%d %H点')").count(:id)
-   
+    @random_check_qty_tmp_size=@random_check_qty_tmp.size
+  # @check_qty_hour_progress = Inventory.where("check_qty != ''").group("date_format(check_time, '%Y-%m-%d %H点')").count(:id)
+   # @random_check_qty_hour_progress = Inventory.random_check_completed.group("date_format(random_check_time, '%Y-%m-%d %H点')").count(:id)
+
+
+    @check_qty_hour_progress = Inventory.where("check_qty != ''").group("date_format(check_time, '%m-%d %H点') as check_time").count(:id)
+    @random_check_qty_hour_progress = Inventory.random_check_completed.group("date_format(random_check_time, '%m-%d %H点')").count(:id)
+
   end
 end
