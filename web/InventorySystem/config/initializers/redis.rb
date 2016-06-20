482abe797f537683e09ca0274971993278fada44
lzding <472339105@qq.com>
2016-06-20 15:04:37 +0800

    fix

--- web/InventorySystem/config/initializers/redis.rb
+++ web/InventorySystem/config/initializers/redis.rb
@@ -5,11 +5,12 @@ default_db=1
 default_db=2 if Rails.env.demo?
 default_db=0 if Rails.env.production?
 
-$redis= Redis::Namespace.new('clearinsight', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => default_db))
+$redis= Redis::Namespace.new('InventorySystem', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => default_db))
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       $redis.client.disconnect
+
       $redis= Redis::Namespace.new('InventorySystem', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => default_db))
     end
   end
